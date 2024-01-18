cbf19fb62362f23d3d85646875ec423455e70f32
Josh Spence <joshspence6@gmail.com>
2024-01-18 11:26:13 -0800

    add modal fade-out

--- src/app/ui/common/modal.tsx
+++ src/app/ui/common/modal.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CloseIcon from "./icons/close-icon";
+import { sleep } from "@/app/lib/util";
 
 export interface ModalProps {
   title: string;
@@ -7,15 +8,29 @@ export interface ModalProps {
   onClose: () => void;
 }
 
+enum ModalState {
+  LOADING = 1,
+  LOADED = 2,
+  CLOSING = 3,
+}
+
 export default function Modal({ title, onClose, children }: ModalProps) {
-  const [hasLoaded, setHasLoaded] = useState(false);
+  const [modalState, setModalState] = useState(ModalState.LOADING);
   useEffect(() => {
-    setHasLoaded(true);
+    if (modalState === ModalState.LOADING) setModalState(ModalState.LOADED);
   });
 
-  const [backgroundOpacity, modalOpacity] = hasLoaded
-    ? ["opacity-50", "opacity-100"]
-    : ["opacity-0", "opacity-0"];
+  const [backgroundOpacity, modalOpacity] =
+    modalState === ModalState.LOADED
+      ? ["opacity-50", "opacity-100"]
+      : ["opacity-0", "opacity-0"];
+
+  const fadeOut = async () => {
+    setModalState(ModalState.CLOSING);
+    await sleep(300);
+    onClose();
+  };
+
   return (
     <div className="absolute y-center top-0 left-0 right-0 bottom-0">
       <div
@@ -33,7 +48,7 @@ export default function Modal({ title, onClose, children }: ModalProps) {
         >
           <div className="bg-zinc-900 px-5 shadow p-2 rounded-t-lg flex justify-between">
             <h4 className="text-4xl text-yellow-100 font-bold">{title}</h4>
-            <button className="y-center  " onClick={onClose}>
+            <button className="y-center  " onClick={fadeOut}>
               <CloseIcon className="h-8 w-8 rounded-full p-1 hover:bg-zinc-700 transition-colors duration-300" />
             </button>
           </div>
